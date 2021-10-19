8c460115f1a06fba3c2bcbf8fc02e6cc8299af5b
axelelise <78152823+axelelise@users.noreply.github.com>
2021-10-19 14:02:36 +0200

    Ajout du login

---
+++ migrations/Version20211019075910.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211019075910 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE pays (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE utilisateur (id INT AUTO_INCREMENT NOT NULL, pays_id INT DEFAULT NULL, username VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_1D1C63B3F85E0677 (username), INDEX IDX_1D1C63B3A6E44244 (pays_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE utilisateur ADD CONSTRAINT FK_1D1C63B3A6E44244 FOREIGN KEY (pays_id) REFERENCES pays (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE utilisateur DROP FOREIGN KEY FK_1D1C63B3A6E44244');
+        $this->addSql('DROP TABLE pays');
+        $this->addSql('DROP TABLE utilisateur');
+    }
+}
--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -4,72 +4,119 @@ namespace App\Entity;
 
 use App\Repository\UtilisateurRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UtilisateurRepository::class)
  */
-class Utilisateur
+class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+  /**
+   * @ORM\Id
+   * @ORM\GeneratedValue
+   * @ORM\Column(type="integer")
+   */
+  private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $Login;
+  /**
+   * @ORM\Column(type="string", length=180, unique=true)
+   */
+  private $username;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $Roles;
+  /**
+   * @ORM\Column(type="json")
+   */
+  private $roles = [];
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $Password;
+  /**
+   * @var string The hashed password
+   * @ORM\Column(type="string")
+   */
+  private $password;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
 
-    public function getLogin(): ?string
-    {
-        return $this->Login;
-    }
+  /**
+   * @deprecated since Symfony 5.3, use getUserIdentifier instead
+   */
+  public function getUsername(): string
+  {
+    return (string)$this->username;
+  }
 
-    public function setLogin(string $Login): self
-    {
-        $this->Login = $Login;
+  public function setUsername(string $username): self
+  {
+    $this->username = $username;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    public function getRoles(): ?string
-    {
-        return $this->Roles;
-    }
+  /**
+   * A visual identifier that represents this user.
+   *
+   * @see UserInterface
+   */
+  public function getUserIdentifier(): string
+  {
+    return (string)$this->username;
+  }
 
-    public function setRoles(string $Roles): self
-    {
-        $this->Roles = $Roles;
+  /**
+   * @see UserInterface
+   */
+  public function getRoles(): array
+  {
+    $roles = $this->roles;
+    // guarantee every user at least has ROLE_USER
+    $roles[] = 'ROLE_USER';
 
-        return $this;
-    }
+    return array_unique($roles);
+  }
 
-    public function getPassword(): ?string
-    {
-        return $this->Password;
-    }
+  public function setRoles(array $roles): self
+  {
+    $this->roles = $roles;
 
-    public function setPassword(string $Password): self
-    {
-        $this->Password = $Password;
+    return $this;
+  }
 
-        return $this;
-    }
+  /**
+   * @see PasswordAuthenticatedUserInterface
+   */
+  public function getPassword(): string
+  {
+    return $this->password;
+  }
+
+  public function setPassword(string $password): self
+  {
+    $this->password = $password;
+
+    return $this;
+  }
+
+  /**
+   * Returning a salt is only needed, if you are not using a modern
+   * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+   *
+   * @see UserInterface
+   */
+  public function getSalt(): ?string
+  {
+    return null;
+  }
+
+  /**
+   * @see UserInterface
+   */
+  public function eraseCredentials()
+  {
+    // If you store any temporary, sensitive data on the user, clear it here
+    // $this->plainPassword = null;
+  }
+
 }
